342b62ac332199b74a043fcfa1d79d9dd5fed35f
Shuvrajeet Das <shuvrajeet17@gmail.com>
2025-03-30 16:19:21 +0530

    updates

--- PA1/mountain_car/main.py
+++ PA1/mountain_car/main.py
@@ -107,90 +107,6 @@ def train_qlearning_softmax(rng, env, qlearning, num_episodes_steps=NUM_SIM_STEP
     return rewards, test_rewards
 
 
-def train_qlearning_epsilon(rng, env, qlearning, num_episodes_steps=NUM_SIM_STEPS, test_interval=TEST_INTERVAL, debug=False, wandb_log=False):
-    rewards = jnp.zeros(num_episodes_steps)
-    test_rewards = jnp.zeros(num_episodes_steps // test_interval)
-    
-
-    @jax.jit
-    def run_episode(carry, episode):
-        rng, qlearning_Q, rewards = carry
-        rng, ep_rng = jax.random.split(rng)
-        state, obs = env.reset(ep_rng)
-        epsilon = jnp.maximum(qlearning.epsilon_end, qlearning.epsilon_start *
-                              (1 - episode / (num_episodes_steps*0.5)))
-
-        def step_fn(step_carry):
-            state, obs, qlearning_Q, ep_rng, episode_reward, done = step_carry
-            ep_rng, action_rng = jax.random.split(ep_rng)
-            (action_rng, qlearning_Q), action = qlearning.act(
-                (action_rng, qlearning_Q), state, epsilon)
-            next_state, next_obs, reward, done = env.step(
-                ep_rng, state, obs, action)
-            qlearning_Q = qlearning.update(
-                qlearning_Q, (state, action, reward, next_state, done))
-
-            episode_reward += reward
-            return (next_state, next_obs, qlearning_Q, ep_rng, episode_reward, done)
-
-        init_carry = (state, obs, qlearning_Q, ep_rng, jnp.float32(0), False)
-        final_carry = jax.lax.while_loop(
-            lambda c: jnp.logical_not(c[5]), step_fn, init_carry)
-        _, _, qlearning_Q, _, episode_reward, _ = final_carry
-        rewards = rewards.at[episode].set(episode_reward)
-        return (rng, qlearning_Q, rewards), episode_reward
-
-    @jax.jit
-    def test_run_episode(carry, episode):
-        rng, qlearning_Q, test_rewards = carry
-        rng, ep_rng = jax.random.split(rng)
-        state, obs = env.reset(ep_rng)
-        epsilon = qlearning.epsilon_end
-
-        def test_step_fn(test_carry):
-            state, obs, qlearning_Q, ep_rng, episode_reward, done = test_carry
-            ep_rng, action_rng = jax.random.split(ep_rng)
-            (action_rng, qlearning_Q), action = qlearning.act(
-                (action_rng, qlearning_Q), state, epsilon)
-            next_state, next_obs, reward, done = env.step(
-                ep_rng, state, obs, action)
-
-            episode_reward += reward
-            return (next_state, next_obs, qlearning_Q, ep_rng, episode_reward, done)
-
-        init_carry = (state, obs, qlearning_Q, ep_rng, jnp.float32(0), False)
-        final_carry = jax.lax.while_loop(
-            lambda c: jnp.logical_not(c[5]), test_step_fn, init_carry)
-        _, _, qlearning_Q, _, episode_reward, _ = final_carry
-        test_rewards = test_rewards.at[episode].set(episode_reward)
-        return (rng, qlearning_Q, test_rewards), episode_reward
-
-    for start in tqdm.tqdm(range(0, num_episodes_steps, test_interval)):
-        end = min(start + test_interval, num_episodes_steps)
-        test_index = start // test_interval
-
-        (rng, qlearning.Q, rewards), _ = jax.lax.scan(
-            run_episode, (rng, qlearning.Q, rewards), jnp.arange(start, end)
-        )
-        (rng, qlearning.Q, test_rewards), _ = jax.lax.scan(
-            test_run_episode, (rng, qlearning.Q,
-                               test_rewards), jnp.array([test_index])
-        )
-        if wandb_log:
-            wandb.log({
-                'train_episode_reward_averaged': np.mean(np.array(rewards[start:end])),
-                'test_episode_reward_averaged': np.mean(np.array(test_rewards[:test_index+1])),
-                'episode': start
-            })
-        print(
-            f'Episode {start}-{end}: Average Reward: {np.mean(np.array(rewards[start:end]))}')
-        print(
-            f'Test Episode {test_index}: Average Reward: {np.mean(np.array(test_rewards[:test_index+1]))}')
-        print()
-
-    return rewards, test_rewards
-
-
 def train_sarsa_epsilon(rng, env, sarsa, num_episodes_steps=NUM_SIM_STEPS, test_interval=TEST_INTERVAL, debug=False, wandb_log=False):
     rewards = jnp.zeros(num_episodes_steps)
     test_rewards = jnp.zeros(num_episodes_steps // test_interval)
@@ -276,89 +192,6 @@ def train_sarsa_epsilon(rng, env, sarsa, num_episodes_steps=NUM_SIM_STEPS, test_
 
     return rewards, test_rewards
 
-
-def train_sarsa_softmax(rng, env, sarsa, num_episodes_steps=NUM_SIM_STEPS, test_interval=TEST_INTERVAL, debug=False, wandb_log=False):
-    rewards = jnp.zeros(num_episodes_steps)
-    test_rewards = jnp.zeros(num_episodes_steps // test_interval)
-    
-
-    @jax.jit
-    def run_episode(carry, episode):
-        rng, sarsa_Q, rewards = carry
-        rng, ep_rng = jax.random.split(rng)
-        state, obs = env.reset(ep_rng)
-        tau = jnp.maximum(sarsa.tau_end, sarsa.tau_start *
-                          (1 - episode / (num_episodes_steps*0.5)))
-
-        def step_fn(step_carry):
-            state, obs, sarsa_Q, ep_rng, episode_reward, done = step_carry
-            ep_rng, action_rng = jax.random.split(ep_rng)
-            (action_rng, sarsa_Q), action = sarsa.act(
-                (action_rng, sarsa_Q), state, tau)
-            next_state, next_obs, reward, done = env.step(
-                ep_rng, state, obs, action)
-            (action_rng, sarsa_Q), next_action = sarsa.act(
-                (action_rng, sarsa_Q), next_state, tau)
-            sarsa_Q = sarsa.update(
-                sarsa_Q, (state, action, reward, next_state, next_action, done))
-
-            episode_reward += reward
-            return (next_state, next_obs, sarsa_Q, ep_rng, episode_reward, done)
-
-        init_carry = (state, obs, sarsa_Q, ep_rng, jnp.float32(0), False)
-        final_carry = jax.lax.while_loop(
-            lambda c: jnp.logical_not(c[5]), step_fn, init_carry)
-        _, _, sarsa_Q, _, episode_reward, _ = final_carry
-        rewards = rewards.at[episode].set(episode_reward)
-        return (rng, sarsa_Q, rewards), episode_reward
-
-    @jax.jit
-    def test_run_episode(carry, episode):
-        rng, sarsa_Q, test_rewards = carry
-        rng, ep_rng = jax.random.split(rng)
-        state, obs = env.reset(ep_rng)
-        tau = sarsa.tau_end
-
-        def test_step_fn(test_carry):
-            state, obs, sarsa_Q, ep_rng, episode_reward, done = test_carry
-            ep_rng, action_rng = jax.random.split(ep_rng)
-            (action_rng, sarsa_Q), action = sarsa.act(
-                (action_rng, sarsa_Q), state, tau)
-            next_state, next_obs, reward, done = env.step(
-                ep_rng, state, obs, action)
-
-            episode_reward += reward
-            return (next_state, next_obs, sarsa_Q, ep_rng, episode_reward, done)
-
-        init_carry = (state, obs, sarsa_Q, ep_rng, jnp.float32(0), False)
-        final_carry = jax.lax.while_loop(
-            lambda c: jnp.logical_not(c[5]), test_step_fn, init_carry)
-        _, _, sarsa_Q, _, episode_reward, _ = final_carry
-        test_rewards = test_rewards.at[episode].set(episode_reward)
-        return (rng, sarsa_Q, test_rewards), episode_reward
-
-    for start in tqdm.tqdm(range(0, num_episodes_steps, test_interval)):
-        end = min(start + test_interval, num_episodes_steps)
-        test_index = start // test_interval
-
-        (rng, sarsa.Q, rewards), _ = jax.lax.scan(
-            run_episode, (rng, sarsa.Q, rewards), jnp.arange(start, end)
-        )
-        (rng, sarsa.Q, test_rewards), _ = jax.lax.scan(
-            test_run_episode, (rng, sarsa.Q, test_rewards), jnp.array([test_index])
-        )
-        if wandb_log:
-            wandb.log({
-                'train_episode_reward_averaged': np.mean(np.array(rewards[start:end])),
-                'test_episode_reward_averaged': np.mean(np.array(test_rewards[:test_index+1])),
-                'episode': start
-            })
-        print(f'Episode {start}-{end}: Average Reward: {np.mean(np.array(rewards[start:end]))}')
-        print(f'Test Episode {test_index}: Average Reward: {np.mean(np.array(test_rewards[:test_index+1]))}')
-        print()
-    
-    return rewards, test_rewards
-
 def sweep_run_sarsa_epsilon():
     sweep_config = {
         "method": "bayes",  # Choose "grid", "random", or "bayes"
@@ -370,7 +203,7 @@ def sweep_run_sarsa_epsilon():
             "episodes":{'values':[10000,20000]},
         }
     }
-    sweep_id = wandb.sweep(sweep_config, project="CartPole_SARSA")
+    sweep_id = wandb.sweep(sweep_config, project="MountainCar_SARSA")
     
     def run_sweep():
         wandb.init(mode='online')
@@ -393,7 +226,7 @@ def sweep_run_qlearning_softmax():
             "episodes":{'values':[10000,20000]},
         }
     }
-    sweep_id = wandb.sweep(sweep_config, project="CartPole_QLEARNING")
+    sweep_id = wandb.sweep(sweep_config, project="MountainCar_QLEARNING")
     
     def run_sweep():
         wandb.init(mode='online')
@@ -408,73 +241,33 @@ def sweep_run_qlearning_softmax():
 
 
 if __name__ == '__main__':
-    sweep_run_qlearning_softmax()
+    # sweep_run_qlearning_softmax()
     # sweep_run_sarsa_epsilon()
-    exit(0)
     
-    os.makedirs('results', exist_ok=True)
+    os.makedirs('results_seeds', exist_ok=True)
     env = Environment()
         
     for i in range(5):
         rng = jax.random.PRNGKey(i)  # Set random seed for reproducibility
         
-        agent = QLearningSoftmax(env=env)
-        rewards, eval_rewards = train_qlearning_softmax(rng, env, agent)
-        np.savetxt(f'results/q_learning_softmax_rewards_{i}.txt', rewards)
-        np.savetxt(f'results/q_learning_softmax_eval_rewards_{i}.txt', eval_rewards)
-        avg_rewards = running_avg_rewards(rewards)
-        eval_avg_rewards = running_avg_rewards(eval_rewards)
-        plt.plot(rewards, label = 'rewards')
-        plt.plot(avg_rewards, label = 'average Reward')
-        plt.xlabel('Episode')
-        plt.ylabel('Total Reward')
-        plt.legend()
-        plt.title('q_learning_softmax_rewards'.replace('_', '-').upper())
-        plt.savefig('q_learning_softmax_rewards.png')
-        plt.close()
+        agent = QLearningSoftmax(env=env, tau_start=1, tau_end=0.02)
+        rewards, eval_rewards = train_qlearning_softmax(rng, env, agent, num_episodes_steps=20_000)
+        np.savetxt(f'results_seeds/q_learning_softmax_rewards_{i}.txt', rewards)
         
-        agent = SARSAEpsGreedy(env=env)
-        rewards, eval_rewards = train_sarsa_epsilon(rng, env, agent)
-        avg_rewards = running_avg_rewards(rewards)
-        eval_avg_rewards = running_avg_rewards(eval_rewards)
-        np.savetxt(f'results/sarsa_epsilon_rewards_{i}.txt', rewards)
-        np.savetxt(f'results/sarsa_epsilon_eval_rewards_{i}.txt', eval_rewards)
-        plt.plot(rewards, label='rewards')
-        plt.plot(avg_rewards, label='average Reward')
-        plt.xlabel('Episode')
-        plt.ylabel('Total Reward')
-        plt.legend()
-        plt.title('sarsa_epsilon_rewards'.replace('_', '-').upper())
-        plt.savefig('sarsa_epsilon_rewards.png')
-        plt.close()
+        agent = SARSAEpsGreedy(env=env, epsilon_start=0.9, epsilon_end=0.01)
+        rewards, eval_rewards = train_sarsa_epsilon(rng, env, agent, num_episodes_steps=20_000)
+        np.savetxt(f'results_seeds/sarsa_epsilon_rewards_{i}.txt', rewards)
         
-        # agent = SARSASoftmax(env=env)
-        # rewards, eval_rewards = train_sarsa_softmax(rng, env, agent)
-        # avg_rewards = running_avg_rewards(rewards)
-        # eval_avg_rewards = running_avg_rewards(eval_rewards)
-        # np.savetxt(f'results/sarsa_softmax_rewards_{i}.txt', rewards)
-        # np.savetxt(f'results/sarsa_softmax_eval_rewards_{i}.txt', eval_rewards)
-        # plt.plot(rewards, label='rewards')
-        # plt.plot(avg_rewards, label='average Reward')
-        # plt.xlabel('Episode')
-        # plt.ylabel('Total Reward')
-        # plt.legend()
-        # plt.title('sarsa_softmax_rewards'.replace('_', '-').upper())
-        # plt.savefig('sarsa_softmax_rewards.png')
-        # plt.close()
-
-        # agent = QLearningEpsGreedy(env=env)
-        # rewards, eval_rewards = train_qlearning_epsilon(rng, env, agent)
-        # avg_rewards = running_avg_rewards(rewards)
-        # eval_avg_rewards = running_avg_rewards(eval_rewards)
-        # np.savetxt(f'results/q_learning_epsilon_rewards_{i}.txt', rewards)
-        # np.savetxt(f'results/q_learning_epsilon_eval_rewards_{i}.txt', eval_rewards)
-        # plt.plot(rewards, label='rewards')
-        # plt.plot(avg_rewards, label='average Reward')
-        # plt.xlabel('Episode')
-        # plt.ylabel('Total Reward')
-        # plt.legend()
-        # plt.title('q_learning_epsilon_rewards'.replace('_', '-').upper())
-        # plt.savefig('q_learning_epsilon_rewards.png')
-        # plt.close()
+    for tau_start in [2,1]:
+        for tau_end in [0.01,0.02]:
+            agent = QLearningSoftmax(env=env, tau_start=tau_start, tau_end=tau_end)
+            rewards, eval_rewards = train_qlearning_softmax(rng, env, agent)
+            np.savetxt(f'results/q_learning_softmax_rewards_{tau_start}_{tau_end}.txt', rewards)
+            
+    for eps_start in [1,0.9]:
+        for eps_end in [0.01,0.02]:
+            agent = SARSAEpsGreedy(env=env, epsilon_start=eps_start, epsilon_end=eps_end)
+            rewards, eval_rewards = train_sarsa_epsilon(rng, env, agent)
+            np.savetxt(f'results/sarsa_epsilon_rewards_{eps_start}_{eps_end}.txt', rewards)
+            
 
